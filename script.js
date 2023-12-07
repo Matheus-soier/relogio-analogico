86f77ac3e0b21d831158b4a90028594b7c6b5e96
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-12-07 16:02:28 -0300

    Projeto completo

---
+++ script.js
@@ -0,0 +1,27 @@
+let digitalElement = document.querySelector('.digital');
+let sElemente = document.querySelector('.p_s');
+let mElemente = document.querySelector('.p_m');
+let hElemente = document.querySelector('.p_h');
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360 / 12) * hour) - 90;
+
+    sElemente.style.transform = `rotate(${sDeg}deg)`;
+    mElemente.style.transform = `rotate(${mDeg}deg)`;
+    hElemente.style.transform = `rotate(${hDeg}deg)`;
+}  
+
+
+let fixZero = (time) => time < 10 ? '0'+time : time;
+
+setInterval(updateClock, 1000);
+updateClock();
